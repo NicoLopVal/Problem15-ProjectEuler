ce8eed18bebe01a5a693f804a601f7b2a5d481f2
nikil <nikil@DESKTOP-73MHGNO>
2022-01-02 20:59:51 -0500

    Add project files.

---
+++ Problem15-ProjectEuler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Problem15_ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 20;
+            int columns = 20;
+            double totalPaths = 1;
+            
+            for(int i = 1; i <= rows; i++)
+            {
+                totalPaths = totalPaths * (rows + i) / i;
+            }
+            
+            Console.WriteLine("The total paths possible for a {0}x{1} grid are: {2}",rows,columns, totalPaths);
+        }
+    }
+}
